950d74fdd161c967ae5ee50a5ea2f33adb611167
Krzysztof Knap <krzknap@gmail.com>
2021-05-11 10:06:01 +0200

    Initial commit

---
+++ src/main/java/com/example/scrapeit/ScrapeItApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrapeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapeItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapeItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scrapeit/ScrapeItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrapeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapeItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
