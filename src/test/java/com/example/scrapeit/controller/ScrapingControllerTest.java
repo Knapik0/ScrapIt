26caf1c4dbdce5a239b02f1ab00ad4f56ef3f3b5
Krzysztof Knap <krzknap@gmail.com>
2021-05-12 23:45:33 +0200

    Generated tests for web layer

---
+++ src/test/java/com/example/scrapeit/controller/ScrapingControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.scrapeit.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScrapingControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listUploadedFiles() {
+    }
+
+    @Test
+    void getFileData() {
+    }
+
+    @Test
+    void uploadFile() {
+    }
+
+    @Test
+    void getLicensesById() {
+    }
+
+    @Test
+    void getLicensesAsCSVById() {
+    }
+}
