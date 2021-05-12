1ff0985d13a7612b3f7109fd6f4365962e5d7255
Krzysztof Knap <krzknap@gmail.com>
2021-05-12 16:43:28 +0200

    File upload PostMapping now returns ResponseEntity

--- src/main/java/com/example/scrapeit/controller/ScrapingController.java
+++ src/main/java/com/example/scrapeit/controller/ScrapingController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -41,10 +40,10 @@ public class ScrapingController {
     }
 
     @PostMapping("/")
-    public String uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
 
         fileService.saveFile(file);
-        return "You successfully uploaded " + file.getOriginalFilename() + "!";
+        return new ResponseEntity<>("You succesfully uploaded file", HttpStatus.OK);
     }
 
     @GetMapping("/licenses/{fileId}")
